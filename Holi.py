94cd5cc27e299975fd8d6f379653a17380c4ef9e
NataliaFarfanEducate <mfarfan@uniempresarial.edu.co>
2025-01-09 15:24:44 -0500

    Clase Holi

---
+++ Holi.py
@@ -0,0 +1,17 @@
+#Hola mundo print 
+print ("Holi perro")
+
+
+"""
+Este es un comentario
+de varias líneas
+"""
+''''
+Este es otro comentario
+de  varias líneas
+'''
+#Consultar el tipo de dato: 
+print (type("Holi perro")) #Tipo "str"
+print (type(5)) #Tipo "int"
+print (type(1.5)) #Tipo "float"
+print (type(True)) #Tipo "boolean"
