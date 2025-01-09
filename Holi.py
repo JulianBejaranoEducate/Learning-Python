baa7a85ec5b11d5edd87be826e21ac7a38a14acd
NataliaFarfanEducate <mfarfan@uniempresarial.edu.co>
2025-01-09 15:50:27 -0500

    Update Holi.py

--- Holi.py
+++ Holi.py
@@ -1,10 +1,10 @@
 #Hola mundo print 
+#Imprime "¡Hola Mundo!" por consola
 print ("Holi perro")
-
-
+print ("Mi nombre: Sara y tengo 20 años")
 """
-Este es un comentario
-de varias líneas
+Un tipo de dato es una forma de clasificar 
+diferentes valores con diferentes "categorias"
 """
 ''''
 Este es otro comentario
@@ -15,3 +15,10 @@ print (type("Holi perro")) #Tipo "str"
 print (type(5)) #Tipo "int"
 print (type(1.5)) #Tipo "float"
 print (type(True)) #Tipo "boolean"
+
+#Pedir nombre al usuario
+print ("Por favor, ingrese su nombre")
+nombre= input()
+print ("¡Hola " + nombre+"!")
+print (2+5)
+print (type (2+5))
