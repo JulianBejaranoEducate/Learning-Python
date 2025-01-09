9b094f999a34585b6734fb5711f16cdb8907e982
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2025-01-09 14:56:42 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,3 +6,8 @@ print("Hola Nena")
 Tenme pasiencia, estoy aprendiendo Ingeniera. :]
 """
 
+'''
+Comentario entre comillas simples, It's possible too.
+¡Hola, nena! ¡Estoy aprendiendo Python! :D
+:]
+'''
