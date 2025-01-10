32aa1a1449fadb330f1752cee1c6d8679938e2fb
NataliaFarfanEducate <mfarfan@uniempresarial.edu.co>
2025-01-10 17:57:11 -0500

    J

---
+++ J00_hello.py
@@ -0,0 +1,76 @@
+# Hola mundo, en Python
+
+print("\nHola Mundo!")
+print('Do you want someyhing?')
+
+"""
+¡Esto tambien es un comentario, de varias líneas!
+Tenme paciencia, estoy aprendiendo Python.
+"""
+
+'''
+Comentario entre comillas simples, It's possible too.
+¡Hola, nena! ¡Estoy aprendiendo Python!
+'''
+
+# Imprime el tipo de dato con "type()"
+# Consultar el tipo de dato:
+print(type("Hola Python")) # Tipo de dato "str" = "String"
+print(type(5)) # Tipo de dato "int" = "Integer"
+print(type(1.5)) # Tipo de dato "float" = "Floating Point"
+print(type(True)) # Tipo de dato "boolean" = "Boolean"
+print(type(7 + 7j)) # Tipo de dato "complex" = "Complex"
+
+
+# Enunciados Ejercicios
+
+# 1.
+print("\n¡Hola Mundo!")
+
+# 2.
+    # El ejercicio numero 1 imprime "¡Hola Mundo!" por consola.
+
+# 3.
+print("Mi nomnbre es Julian Bejarano y tengo", 22, "años.")
+
+# 4.
+print(type("Julian Bejarano"))
+print(type(22))
+print(type(1.4))
+
+# 5.
+"""
+Los tipos de datos en Pyhton 
+son una forma de clasificar diferentes 
+valores con diferentes "categorias".
+"""
+
+# 6.
+print("Hola", "Mundo", "Python")
+
+# 7.
+nombre = "Julian Bejarano"
+edad = 22
+
+print("¡Hola", nombre + "!", "Tienes", edad, "años.")
+"""
+Cuando se concatena con el signo "+", no deja espacio entre las palabras.
+Cuando se concatena con la coma ",", deja espacio entre las palabras.
+"""
+
+# 8.
+print("Por favor, ingresa tu nombre")
+nombre = input()
+print("¡Hola", nombre + "!", "Bienvenido a Python.")
+
+# 9.
+a = 7
+b = 7
+print(a + b)
+print(type(a + b))
+
+# 10.
+    # Declaro una variable "a" con el valor 7 y otra variable "b" con el valor
+    # Depues imprimo la suma de ambas variables y el tipo de dato de la suma.
+
+
---
+++ J02_Operadores.py
@@ -0,0 +1,3 @@
+"""Hi niño
+como estas?
+"""
