bda60f6ecf4eaad90cd36afa3eb70c8f10cb374d
Julian Bejarano <jbejarano@uniempresarial.edu.co>
2025-01-11 23:11:57 -0500

    New commits

---
+++ J03_strings.py
@@ -0,0 +1,105 @@
+# String
+
+""" Atajos en python con vsc """
+# Ctrl + K + C -> Comentar
+# Ctrl + K + U -> Descomentar
+# Ctrl + Shift + P -> Buscar comandos
+# Ctrl + Shift + L -> Seleccionar todas las coincidencias
+# Ctrl + D -> Seleccionar la siguiente coincidencia
+# Shift + Alt + teclad arriba/abajo -> Duplicar línea
+# Ctrl + Shift + K -> Eliminar línea
+# Alt + flecha arriba/abajo -> Mover línea
+
+my_string = "Mi String"
+my_other_string = "Mi otro String"
+
+print(len(my_string)) # Longitud de la cadena   
+print(len(my_other_string)) # Longitud de la cadena
+
+print(my_string + " " + my_other_string) # Concatenación
+
+my_new_line_string = "Este es un String \ncon un salto de línea"
+print(my_new_line_string) # Salto de línea
+
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string) # Tabulación
+
+my_scape_string = "\\Este es un String \\ escapado"
+print(my_scape_string) # Escapado
+
+
+''' Formateo '''
+nombre, apellido, age = "Julian", "Bejarano", 22
+print("Mi nomnbre es %s %s y tengo %d años" % (nombre, apellido, age)) # Formateo con %
+print("Mi nomnbre es {} {} y tengo {} años".format(nombre, apellido, age)) # Formateo con format
+# No se recomienda el formateo con concatenación
+print("Mi nombre es " + nombre + " " + apellido + " y tengo " + str(age) + " años") # Concatenación
+print(f"Mi nombre es {nombre} {apellido} y tengo {age} años") # Formateo con f
+
+
+''' Desenpaquetado de caracteres '''
+language = "Python"
+a, b, c, d, e, f = language
+print(a) # P
+print(b) # y
+
+
+''' División '''
+language_slice = language[0:3] # [start:stop]
+print(language_slice)
+
+language_slice = language[1:] # [start:]
+print(language_slice)
+
+language_slice = language[-3] # [-index]
+print(language_slice)
+
+language_slice = language[0:6:2]
+print(language_slice)
+
+
+''' Reverse '''
+reversed_language = language[::-1] # [start:stop:step]
+print(reversed_language)
+
+
+''' Funciones '''
+idioma = "python"
+print(idioma.capitalize()) # Capitaliza la primera letra
+print(idioma.upper()) # Mayúsculas
+print(idioma.lower()) # Minúsculas
+print(idioma.count("t")) # Cuenta
+print(idioma.find("t")) # Encuentra
+print(idioma.isnumeric()) # Es numérico
+print("1".isnumeric()) # Es numérico
+print(idioma.replace("P", "J")) # Reemplaza
+print(idioma.count("P")) # Cuenta
+
+print(idioma.upper().isupper())
+print(idioma.lower().islower())
+
+print(idioma.startswith("py")) # Empieza con
+print(idioma.endswith("on")) # Termina con
+
+
+# Enunciados Ejercicios
+
+# 1. Declara una variable "text" con la frase <Aprendiendo Python> y luego imprime la longitud de la cadena usando "len()".
+
+# 2. Concatena dos cadenas: "Hola" y "Python", y muestra el resultadp en una sola linea.
+
+# 3. Crea una cadena que incluya un salto de linea, y luego imprimela para ver el resultado.
+
+# 4. Usa el formateo de cadenas con "f-strings" para imprimir tu nombre, apellido y edad en una cadena de texto.
+
+# 5. Desempaqueta los caracteres de la palabra "Python" en variables separadas y luego imprimelos uno por uno.
+
+# 6. Extrae un "slice" de la palabra "Programacion" para obtener los caracteres desde la posicion 3 hasta la 7.
+
+# 7. Inivierte la cadena "Python" usando slicing y muestra el resulatado.
+
+# 8. Convierte la cadena "Aprendiendo python" en mayúsculas usando el metodo adecuado e imprimela.
+
+# 9. Cuenta cuantas veces aparce la letra "n" en la cadena "Programacion en Python".
+
+# 10. Verifica si la cadena "12345" es numerica usando el metodo adecuado e imprime el resultado.
---
+++ J04_tuplas.py
@@ -0,0 +1,34 @@
+# Tuplas
+
+""" Atajos en python con vsc """
+# Ctrl + K + C -> Comentar
+# Ctrl + K + U -> Descomentar
+# Ctrl + Shift + P -> Buscar comandos
+# Ctrl + Shift + L -> Seleccionar todas las coincidencias
+# Ctrl + D -> Seleccionar la siguiente coincidencia
+# Shift + Alt + teclad arriba/abajo -> Duplicar línea
+# Ctrl + Shift + K -> Eliminar línea
+# Alt + flecha arriba/abajo -> Mover línea
+
+
+# Enunciado Ejercicios
+
+# 1. Crea una tupla con los valores (10, 20, 30, 40, 50) e imprimela.
+
+# 2. Accede al segundo elemento de la tupla (100, 200, 300, 400, 500) y muestralo.
+
+# 3. Intenta modificar el primer elemento de la tupla (1, 2, 3) a 10 y observa el resultado. 
+
+# 4. Cuenta cuántas veces aparece el número 3 en la tupla (1, 2, 3, 3, 4, 5, 3).
+
+# 5. Encuentra el índice de la primera aparición de la cadena "Python" en la tupla ("Java", "Python", "JavaScript", "Python").
+
+# 6. Concatena dos tuplas: (1, 2, 3) y (4, 5, 6) e imprime la tupla resultante. 
+
+# 7. Crea una subtupla con los elementos desde la posición 2 hasta la 4 (sin incluir la 4) de la tupla (10, 20, 30, 40, 50).
+
+# 8. Convierte la tupla ("rojo", "verde", "azul") en una lista, cambia el segundo elemento a "amarillo" y vuelve a convertirla en una tupla. Imprime la tupla resultante. 
+
+# 9. Elimina una tupla llamada my_tuple usando del y luego intenta imprimirla para ver el resultado. 
+
+# 10. Crea una tupla con un solo elemento (el número 100) e imprímela. Asegúrate de usar la sintaxis correcta para crear una tupla con un solo elemento. 
