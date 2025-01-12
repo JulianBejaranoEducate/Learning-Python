1f3de999e9d41b338c602f5127b4245b7d5611ed
NataliaFarfanEducate <mfarfan@uniempresarial.edu.co>
2025-01-11 22:40:53 -0500

    Update 07_Diccionarios.py

--- 07_Diccionarios.py
+++ 07_Diccionarios.py
@@ -48,11 +48,70 @@ print(my_dict.values())
 my_list =["Nombre", 1, "Piso"]
 
 #Nuevo diccionario sin valores
+print("-------------------------------------------------------")
 my_new_dict = my_dict.fromkeys(my_list)#pasar claves
 print(my_new_dict)
-my_new_dict = my_dict.fromkeys("Nombre", 1, "Piso")#pasar claves
+#
+my_new_dict = my_dict.fromkeys(("Nombre", 1, "Piso"))
 print(my_new_dict)
-my_new_dict = my_dict.fromkeys(my_dict)#pasar claves
+# Meter a todas las claves el valor(es) qu ingresamos
+my_new_dict = my_dict.fromkeys(my_dict, ("Malory", "Farfan"))
+print(my_new_dict)
+#Toma las claves
+print(list(my_new_dict))
+print(tuple(my_new_dict))
+print(set(my_new_dict))
+
+# 1. Crea un diccionario con las claves name, age, y country, asignando valores a cada una. Imprime el diccionario.
+my_dict = {
+    "Name": "Natalia",
+    "Age": 17,
+    "Country": "Londres"
+}
+print(my_dict)
+
+# 2. Accede al valor de la clave name en el diccionario.
+print(my_dict["Name"])
+
+# 3. AÃ±ade una nueva clave job con el valor "Programador" al diccionario del punto anterior. Imprime el diccionario actualizado.
+my_dict["Job"] = "Programador"
+print(my_dict)
+
+# 4. Modifica el valor de la clave age en el diccionario para que sea 38. Imprime el diccionario actualizado.
+my_dict["Age"] =38
+print(my_dict)
+
+# 5. Elimina la clave country del diccionario e imprime el diccionario resultante.
+del my_dict["Country"]
+print(my_dict)
+
+# 6. Crea un diccionario donde las claves sean nÃºmeros del 1 al 5 y los valores sean sus cuadrados (ejemplo: 1: 1, 2: 4, ...).
+my_other_dict={
+    1 : 1,
+    2 : 4,
+    3 : 9,
+    4 : 16,
+    5 : 25
+}
+print(my_other_dict)
+
+# 7. Verifica si la clave age estÃ¡ presente en el diccionario {"name": "Brais", "age": 37, "country": "Galicia"}.
+my_dict = {
+    "name": "Brais",
+    "age": 37,
+    "country": "Galicia"
+    }
+print(my_dict)
+
+# 8. Imprime solo las claves del diccionario.
+print(my_dict.keys())
+
+# 9. Convierte las claves del diccionario en una lista e imprime la lista resultante.
+print(list(my_dict))
+
+# 10. Crea un nuevo diccionario a partir de una lista de claves ["name", "age", "job"] 
+# usando fromkeys(), asignando a todas las claves el valor "Desconocido".
+my_dict = ["name", "age", "job"]
+my_new_dict = dict.fromkeys(my_dict,"Desconocido")
 print(my_new_dict)
 
-#quede en minuto 14:44
